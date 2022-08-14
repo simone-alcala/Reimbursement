5981cc52724470038845a6882da6ff80b58c1b35
simone-alcala <simone.alcala@gmail.com>
2022-08-14 20:30:01 -0300

    Creating project

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
+import './setup.js';
 import app from './app.js';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const PORT = +process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Running on ${PORT}`));
